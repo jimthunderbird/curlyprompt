81502b790eb555f632c919276f736c50c6148c2d
Jim Huang <jim.javathunderbird@gmail.com>
2026-01-01 06:59:15 -0800

    make sure all elements in html are loaded

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
             }
             foreach ($numbers as $number) {
                 if ($number % 2 !== 0) {
-                    echo "<li data-number=\"$number\">odd number $number</li>";
+                    echo '<li data-number="' . $number . '" data-type="odd">odd number ' . $number . '</li>';
                 } else {
-                    echo "<li data-number=\"$number\">even number $number</li>";
+                    echo '<li data-number="' . $number . '" data-type="even">even number ' . $number . '</li>';
                 }
             }
             ?>
@@ -28,42 +28,43 @@
     </div>
 
     <script>
-    {
-        const numbersList = document.getElementById('numbers-list');
-        let numbersListCounter = 0;
+    document.addEventListener('DOMContentLoaded', function() {
+        {
+            const numbersList = document.getElementById('numbers-list');
+            let listCounter = 0;
 
-        numbersList.addEventListener('click', function(e) {
-            if (e.target.tagName === 'LI') {
-                numbersListCounter++;
-                const number = e.target.getAttribute('data-number');
-                const displayer = document.getElementById('displayer');
-                displayer.innerHTML = `clicked on odd number ${number} in the list, current counter ${numbersListCounter}`;
-            }
-        });
+            numbersList.addEventListener('click', function(e) {
+                if (e.target.tagName === 'LI') {
+                    listCounter++;
+                    const number = e.target.getAttribute('data-number');
+                    const type = e.target.getAttribute('data-type');
+                    document.getElementById('displayer').innerHTML = `clicked on ${type} number ${number} in the list, current counter ${listCounter}`;
+                }
+            });
 
-        numbersList.addEventListener('mouseover', function(e) {
-            if (e.target.tagName === 'LI') {
-                e.target.style.cursor = 'pointer';
-            }
-        });
+            numbersList.addEventListener('mouseover', function(e) {
+                if (e.target.tagName === 'LI') {
+                    e.target.style.cursor = 'pointer';
+                }
+            });
 
-        numbersList.addEventListener('mouseout', function(e) {
-            if (e.target.tagName === 'LI') {
-                e.target.style.cursor = 'default';
-            }
-        });
-    }
+            numbersList.addEventListener('mouseout', function(e) {
+                if (e.target.tagName === 'LI') {
+                    e.target.style.cursor = 'default';
+                }
+            });
+        }
 
-    {
-        const button1 = document.getElementById('button1');
-        let button1Counter = 0;
+        {
+            const button1 = document.getElementById('button1');
+            let buttonCounter = 0;
 
-        button1.addEventListener('click', function() {
-            button1Counter++;
-            const displayer = document.getElementById('displayer');
-            displayer.innerHTML = `buton 1 counter ${button1Counter}`;
-        });
-    }
+            button1.addEventListener('click', function() {
+                buttonCounter++;
+                document.getElementById('displayer').innerHTML = `button 1 counter ${buttonCounter}`;
+            });
+        }
+    });
     </script>
 </body>
 </html>
