5ccbc9dc0594f25201f277abc29114788d2163d0
Jim Huang <jim.javathunderbird@gmail.com>
2026-01-01 10:42:15 -0800

    generate random numbers in place

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="demo,simple,php">
+    <meta name="keywords" content="demo, simple, php">
     <title>clicking components demo</title>
     <link rel="stylesheet" href="/web.css">
     <script src="/web1.js"></script>
@@ -13,16 +13,19 @@
     <div id="container">
         <button id="button1">button 1</button>
         <ul id="numbers-list">
-            <?php
-            $numbers = \ManagerSpaces\NumberManager::getNumbersRangingFrom(1, 150);
-            foreach ($numbers as $number) {
-                if ($number % 2 !== 0) {
-                    echo "<li data-number='{$number}' data-type='odd'>odd number {$number}</li>";
-                } else {
-                    echo "<li data-number='{$number}' data-type='even'>even number {$number}</li>";
-                }
-            }
-            ?>
+<?php
+$numbers = [];
+for ($i = 0; $i < 10; $i++) {
+    $numbers[] = rand(1, 150);
+}
+foreach ($numbers as $number) {
+    if ($number % 2 !== 0) {
+        echo '<li data-number="' . $number . '" data-type="odd">odd number ' . $number . '</li>' . "\n";
+    } else {
+        echo '<li data-number="' . $number . '" data-type="even">even number ' . $number . '</li>' . "\n";
+    }
+}
+?>
         </ul>
         <div id="displayer"></div>
     </div>
--- web1.js
+++ web1.js
@@ -1,47 +1,43 @@
-(function() {
-    'use strict';
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        {
-            const button1 = document.getElementById('button1');
-            let button1Counter = 0;
-            const displayer = document.getElementById('displayer');
-            
-            if (button1) {
-                button1.addEventListener('click', function() {
-                    button1Counter++;
-                    displayer.innerHTML = `button 1 counter ${button1Counter}`;
-                });
-            }
+document.addEventListener('DOMContentLoaded', function() {
+    {
+        const button1 = document.getElementById('button1');
+        let button1Counter = 0;
+        const displayer = document.getElementById('displayer');
+
+        if (button1) {
+            button1.addEventListener('click', function() {
+                button1Counter++;
+                displayer.innerHTML = 'buton 1 counter ' + button1Counter;
+            });
         }
-        
-        {
-            const numbersList = document.getElementById('numbers-list');
-            let listCounter = 0;
-            const displayer = document.getElementById('displayer');
-            
-            if (numbersList) {
-                numbersList.addEventListener('click', function(event) {
-                    if (event.target.tagName === 'LI') {
-                        listCounter++;
-                        const number = event.target.getAttribute('data-number');
-                        const type = event.target.getAttribute('data-type');
-                        displayer.innerHTML = `clicked on ${type} number ${number} in the list, current counter ${listCounter}`;
-                    }
-                });
-                
-                numbersList.addEventListener('mouseover', function(event) {
-                    if (event.target.tagName === 'LI') {
-                        event.target.style.cursor = 'pointer';
-                    }
-                });
-                
-                numbersList.addEventListener('mouseout', function(event) {
-                    if (event.target.tagName === 'LI') {
-                        event.target.style.cursor = 'default';
-                    }
-                });
-            }
+    }
+
+    {
+        const numbersList = document.getElementById('numbers-list');
+        let listCounter = 0;
+        const displayer = document.getElementById('displayer');
+
+        if (numbersList) {
+            numbersList.addEventListener('click', function(e) {
+                if (e.target && e.target.nodeName === 'LI') {
+                    const number = e.target.getAttribute('data-number');
+                    const type = e.target.getAttribute('data-type');
+                    listCounter++;
+                    displayer.innerHTML = 'clicked on ' + type + ' number ' + number + ' in the list, current counter ' + listCounter;
+                }
+            });
+
+            numbersList.addEventListener('mouseover', function(e) {
+                if (e.target && e.target.nodeName === 'LI') {
+                    e.target.style.cursor = 'pointer';
+                }
+            });
+
+            numbersList.addEventListener('mouseout', function(e) {
+                if (e.target && e.target.nodeName === 'LI') {
+                    e.target.style.cursor = 'default';
+                }
+            });
         }
-    });
-})();
+    }
+});
