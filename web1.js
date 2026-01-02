de5d187c8c0bc88942702cafdc73ee5b47e1a5b6
Jim Huang <jim.javathunderbird@gmail.com>
2026-01-02 10:24:06 -0800

    add spa demo

--- web1.js
+++ web1.js
@@ -1,43 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    {
-        const button1 = document.getElementById('button1');
-        let button1Counter = 0;
-        const displayer = document.getElementById('displayer');
+(() => {
+    let button1Counter = 0;
+    let listCounter = 0;
 
-        if (button1) {
-            button1.addEventListener('click', function() {
+    document.addEventListener('DOMContentLoaded', () => {
+        {
+            const button1 = document.getElementById('button1');
+            const displayer = document.getElementById('displayer');
+            
+            button1.addEventListener('click', () => {
                 button1Counter++;
-                displayer.innerHTML = 'buton 1 counter ' + button1Counter;
+                displayer.innerHTML = `buton 1 counter ${button1Counter}`;
             });
         }
-    }
 
-    {
-        const numbersList = document.getElementById('numbers-list');
-        let listCounter = 0;
-        const displayer = document.getElementById('displayer');
+        {
+            const numbersList = document.getElementById('numbers-list');
+            const displayer = document.getElementById('displayer');
 
-        if (numbersList) {
-            numbersList.addEventListener('click', function(e) {
-                if (e.target && e.target.nodeName === 'LI') {
+            numbersList.addEventListener('click', (e) => {
+                if (e.target.tagName === 'LI') {
                     const number = e.target.getAttribute('data-number');
-                    const type = e.target.getAttribute('data-type');
                     listCounter++;
-                    displayer.innerHTML = 'clicked on ' + type + ' number ' + number + ' in the list, current counter ' + listCounter;
+                    displayer.innerHTML = `clicked on odd number ${number} in the list, current counter ${listCounter}`;
                 }
             });
 
-            numbersList.addEventListener('mouseover', function(e) {
-                if (e.target && e.target.nodeName === 'LI') {
+            numbersList.addEventListener('mouseover', (e) => {
+                if (e.target.tagName === 'LI') {
                     e.target.style.cursor = 'pointer';
                 }
             });
 
-            numbersList.addEventListener('mouseout', function(e) {
-                if (e.target && e.target.nodeName === 'LI') {
+            numbersList.addEventListener('mouseout', (e) => {
+                if (e.target.tagName === 'LI') {
                     e.target.style.cursor = 'default';
                 }
             });
         }
-    }
-});
+    });
+})();
