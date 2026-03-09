ca93cf43f2f3ba0e18cb00a2a5650427437c009f
jim huang <jim.javathunderbird@gmail.com>
2026-03-09 03:04:58 -0700

    update path

--- examples/aider_local_llm/run.sh
+++ examples/aider_local_llm/run.sh
@@ -1,4 +1,4 @@
-aider ./ \
+aider ./spa_php.php \
       --subtree-only --model ollama_chat/qwen3-coder:30b \
       --set-env OLLAMA_API_BASE=http://localhost:11434 \
       --msg "$(cat spa_php.prompt)" \
