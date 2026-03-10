143c0564c16b09126661796e84c188604f8cc717
jim huang <jim.javathunderbird@gmail.com>
2026-03-09 17:48:43 -0700

    solve aider path

---
+++ examples/hello_world/run_aider.sh
@@ -0,0 +1,8 @@
+PROMPT_FILE=./spec.prompt
+CUR_DIR=$(dirname "$(realpath $PROMPT_FILE)")
+PROMPT=$(sed "s|\$CUR_DIR|$CUR_DIR|g" $PROMPT_FILE)
+
+aider --subtree-only --model ollama_chat/qwen3-coder:30b \
+      --set-env OLLAMA_API_BASE=http://localhost:11434 \
+      --msg "$PROMPT" \
+      --yes
