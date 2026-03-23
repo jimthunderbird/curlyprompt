f9e4b91a590740ea2509dd498f6192f53d328951
jim huang <jim.javathunderbird@gmail.com>
2026-03-23 02:48:29 -0700

    add gitignore

--- examples/local_skills/final_action.py
+++ examples/local_skills/final_action.py
@@ -1,4 +1,12 @@
-from skills.dictionary.search import SKILL as skills_dictionary_search
-word = "abandon"
-meaning = skills_dictionary_search.run(word)
-print(meaning)
+from skills.wikipedia.search import SKILL as skills_wikipedia_search
+
+keyword = "current president of united states"
+num_of_results = 1
+results = skills_wikipedia_search.run(keyword, num_of_results)
+title = results[0]['title']
+url = results[0]['url']
+extract = results[0]['extract']
+
+print(f"Title: {title}")
+print(f"URL: {url}")
+print(f"Summary: {extract}")
