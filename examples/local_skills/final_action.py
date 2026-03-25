ef4ed1ea083450ab04d8d2e05849c5141d817c39
jim huang <jim.javathunderbird@gmail.com>
2026-03-25 11:35:00 -0700

    update skill

--- examples/local_skills/final_action.py
+++ examples/local_skills/final_action.py
@@ -3,13 +3,18 @@ from skills.wikipedia.search import SKILL as skills_wikipedia_search
 question = "what is donald trump's birthday"
 keyword = "Donald Trump"
 num_of_results = 1
-results = skills_wikipedia_search.run(question, keyword, num_of_results)
+save_to_file = "result1.txt"
+
+results = skills_wikipedia_search.run(question, keyword, num_of_results, save_to_file)
 title = results[0]['title']
 url = results[0]['url']
 extract = results[0]['extract']
 
-# Save to file
-with open("result.txt", mode="w", encoding="utf-8") as f:
-    f.write(f"Title: {title}\n")
-    f.write(f"URL: {url}\n")
-    f.write(f"Extract: {extract}\n")
+# Read file content from url
+with open(save_to_file, 'r') as f:
+    content = f.read()
+
+# Construct prompt
+prompt = f"forget about your previous knowledge, based only on the following facts, answer the question: {question}\nfacts {{\n{content}\n}}"
+
+# Send prompt to ollama model gemma3:latest with streaming
