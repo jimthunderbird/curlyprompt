b7f59bd42c93a59584717d52bef450eef4e1a7b2
jim huang <jim.javathunderbird@gmail.com>
2026-03-26 00:05:37 -0700

    improve related entity research

--- examples/local_skills/final_action.py
+++ examples/local_skills/final_action.py
@@ -2,7 +2,7 @@ import asyncio
 from skills.wikipedia.search import SKILL as skills_wikipedia_search
 
 async def main():
-    question = "are they related?"
+    question = "are salt, sugar, salted fish related to cancer"
     entities = ["salt", "sugar", "salted fish", "cancer"]
     await skills_wikipedia_search.run_related(question, entities)
 
