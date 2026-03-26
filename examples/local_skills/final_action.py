8566343a35da9f7f73325a98fcbf317c77cd7004
jim huang <jim.javathunderbird@gmail.com>
2026-03-26 08:04:59 -0700

    add skill prompt

--- examples/local_skills/final_action.py
+++ examples/local_skills/final_action.py
@@ -2,8 +2,10 @@ import asyncio
 from skills.wikipedia.search import SKILL as skills_wikipedia_search
 
 async def main():
-    question = "are salt, sugar, salted fish related to cancer"
-    entities = ["salt", "sugar", "salted fish", "cancer"]
-    await skills_wikipedia_search.run_related(question, entities)
+    keyword = "donald trump"
+    question = "what is donald trump's birthday"
+    num_of_results = 1
+    save_to_file = None
+    await skills_wikipedia_search.run(question, keyword, num_of_results, save_to_file)
 
 asyncio.run(main())
